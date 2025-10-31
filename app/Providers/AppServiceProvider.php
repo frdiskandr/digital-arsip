571ce27d0c600c2c9f67774975dabad339f4d8dd
frdiskandr <fariditb159@gmail.com>
2025-10-31 13:57:45 +0700

    add config for mysql (string 767 bytes)

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Filament\Support\Facades\FilamentView;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\HtmlString;
 
 class AppServiceProvider extends ServiceProvider
@@ -21,6 +22,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        Schema::defaultStringLength(191);
         FilamentView::registerRenderHook(
             'panels::head.end',
             fn (): HtmlString => new HtmlString('<script src="https://cdn.tailwindcss.com"></script>'),
