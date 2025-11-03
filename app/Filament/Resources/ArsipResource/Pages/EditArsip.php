f3d51c9e1058c138d82d2e51aeeb1ce0e9e94e9b
frdiskandr <fariditb159@gmail.com>
2025-11-03 14:46:45 +0700

    add fitur redirect

--- app/Filament/Resources/ArsipResource/Pages/EditArsip.php
+++ app/Filament/Resources/ArsipResource/Pages/EditArsip.php
@@ -9,6 +9,10 @@ use Filament\Resources\Pages\EditRecord;
 class EditArsip extends EditRecord
 {
     protected static string $resource = ArsipResource::class;
+     protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
 
     protected function getHeaderActions(): array
     {
